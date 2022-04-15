483b4a36683c545f30bea4e0e3e9417253797780
Seunggu Kim <reizel@naver.com>
2022-04-16 07:15:37 +0900

    03-02-01

--- appmain/__init__.py
+++ appmain/__init__.py
@@ -4,3 +4,6 @@ app = Flask(__name__)
 
 from appmain.routes import main
 app.register_blueprint(main)
+
+from appmain.user.routes import user
+app.register_blueprint(user)
--- appmain/routes.py
+++ appmain/routes.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, send_from_directory
+
+from appmain import app
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 @main.route('/home')
 def home():
-    response = 'pyBook main page'
-
-    return response
+    return send_from_directory(app.root_path, 'templates/index.html')
