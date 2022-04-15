bea3e129dcd0e0f57db1e7b924fc4ab3592db0b0
Seunggu Kim <reizel@naver.com>
2022-04-16 07:20:34 +0900

    03-02-01

--- appmain/__init__.py
+++ appmain/__init__.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 from appmain.routes import main
 app.register_blueprint(main)
 
-from appmain.user.routes import user
-app.register_blueprint(user)
+# from appmain.user.routes import user
+# app.register_blueprint(user)
