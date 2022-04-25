c6cb5b0b01ed2f7a72cdc97025e32131b7bf8e45
Seunggu Kim <reizel@naver.com>
2022-04-26 07:58:29 +0900

    03-02-03

---
+++ appmain/static/js/navbar.js
@@ -0,0 +1,29 @@
+let signup = document.querySelector("#signup_link");
+let signin = document.querySelector("#signin_link");
+let signout = document.querySelector("#signout_link");
+let myinfo = document.querySelector("#myinfo_link");
+
+function showAndHideNavbarMenu() {
+    let authtoken = window.sessionStorage.getItem("authtoken");
+    console.log('index.window.onload().authtoken:%s', authtoken);
+
+    if(authtoken){
+        signup.style.display = "none";
+        signin.style.display = "none";
+    }
+    else{
+        signout.style.display = "none";
+        myinfo.style.display = "none";
+    }
+}
+
+window.addEventListener("load", showAndHideNavbarMenu);
+
+function signOutHandler() {
+    window.sessionStorage.removeItem("authtoken");
+
+    let url = '/home';
+    window.location.replace(url);
+}
+
+signout.addEventListener("click", signOutHandler);
