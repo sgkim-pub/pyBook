9cc3e97cc22b91efdb9402b2916f79a51561b8e8
Seunggu Kim <reizel@naver.com>
2022-05-02 09:00:15 +0900

    04-01

--- appmain/article/routes.py
+++ appmain/article/routes.py
@@ -11,3 +11,45 @@ article = Blueprint('article', __name__)
 def createArticlePage():
     return send_from_directory(app.root_path, 'templates/create_article.html')
 
+@article.route('/api/article/create', methods=['POST'])
+def createArticle():
+
+    headerData = request.headers
+    data = request.form
+    files =request.files
+
+    authToken = headerData.get("authtoken")
+    print('createArticle.authtoken', authToken)
+
+    if authToken:
+        isValid = verifyJWT(authToken)
+
+        if isValid:
+            print('createArticle.isValid:', isValid)
+            token = getJWTContent(authToken)
+            username = token["username"]
+
+            category = data.get("category")
+            title = data.get("title")
+            desc = data.get("desc")
+
+            if files:
+                print('createArticle.files', files)
+
+            print('createArticle.username', username)
+            print('createArticle.category', category)
+            print('createArticle.title', title)
+            print('createArticle.desc', desc)
+
+            # conn = sqlite3.connect('pyBook.db')
+            # cursor = conn.cursor()
+            #
+            #     if cursor:
+        else:
+            pass
+    else:
+        pass
+
+    payload = {"success": False}
+
+    return make_response(jsonify(payload), 200)
