a76414f154136831ef5e770718d6f800c72067a5
Seunggu Kim <reizel@naver.com>
2022-05-04 20:01:38 +0900

    04-03

--- appmain/article/routes.py
+++ appmain/article/routes.py
@@ -119,3 +119,74 @@ def getRecentArticles():
         payload = {"success": True, "articles": recentArticleDics}
 
     return make_response(jsonify(payload), 200)
+
+@article.route('/display_article/<int:articleNo>', methods=['GET'])
+def displayArticlePage(articleNo):
+    # return send_from_directory(app.root_path, 'templates/display_article.html')
+    # return make_response(send_from_directory(app.root_path, 'templates/display_article.html'),
+    #                      jsonify({0: 1}), 200)
+
+    response = make_response(send_from_directory(app.root_path, 'templates/display_article.html'), 200)
+    # response.data["articleNo"] = articleNo
+    response.set_data({"articleNo": articleNo})
+
+    return response
+
+def translateCategory(catId):
+    category = '미분류'
+
+    if catId == 0:
+        category = '인문'
+    elif catId == 1:
+        category = '사회과학'
+    elif catId == 2:
+        category = '자연과학'
+    elif catId == 3:
+        category = '의학'
+    elif catId == 4:
+        category = '경제/경영'
+    elif catId == 5:
+        category = '공학'
+    elif catId == 6:
+        category = '음악'
+    elif catId == 7:
+        category = '미술'
+    elif catId == 8:
+        category = '기타'
+    else:
+        category = '미분류'
+
+    return category
+
+@article.route('/api/article/display/<int:articleNo>', methods=['GET', 'POST'])
+def displayArticle(articleNo):
+
+    payload = {"success": False}
+
+    conn = sqlite3.connect('pyBook.db')
+    cursor = conn.cursor()
+
+    if cursor:
+        SQL = 'SELECT author, title, category, description, price, picture FROM articles WHERE articleNo=?'
+        cursor.execute(SQL, articleNo)
+        result = cursor.fetchone()
+
+        cursor.close()
+    conn.close()
+
+    print('displayArticle().article:', result)
+
+    if result:
+        if result[5]:
+            picFilePath = 'pics/' + result[0] + '/' + result[5]
+            picURL = url_for('static', filename=picFilePath, _external=True)
+        else:
+            picURL = None
+
+        article = {"author": result[0], "title": result[1], "category": translateCategory(result[2]),
+                   "description": result[3], "price": result[4], "picture": picURL}
+        payload = {"success": True, "article": article}
+    else:
+        payload = {"success": True, "article": None}
+
+    return make_response(jsonify(payload), 200)
