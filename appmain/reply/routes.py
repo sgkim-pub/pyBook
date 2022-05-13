6b7df884a7f8e43a33ee219ce1d43b1612f34c61
Seunggu Kim <reizel@naver.com>
2022-05-14 08:01:50 +0900

    04-05

--- appmain/reply/routes.py
+++ appmain/reply/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, make_response
+from flask import Blueprint, request, make_response, jsonify
 
 import sqlite3
 
@@ -23,8 +23,31 @@ def leaveReply():
         if isValid:
             token = getJWTContent(authToken)
             username = token["username"]
+
+            articleNo = data.get("articleNo")
+            reply = data.get("reply")
+
+            conn = sqlite3.connect('pyBook.db')
+            cursor = conn.cursor()
+
+            if cursor:
+                SQL = 'INSERT INTO replies (author, description, targetArticle) VALUES(?, ?, ?)'
+                cursor.execute(SQL, (username, reply, articleNo))
+                conn.commit()
+
+                SQL = 'SELECT * FROM replies'
+                cursor.execute(SQL)
+                rows = cursor.fetchall()
+                for row in rows:
+                    print(row)
+
+                cursor.close()
+            conn.close()
+
+            payload = {"success": True}
         else:   # if isValid:
             pass
     else:   # if authToken:
         pass
-    
+
+    return make_response(jsonify(payload), 200)
